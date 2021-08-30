fa728a86e892f5f10d6a47b014e0b9b41a2cfd67
Dewey <deweyshi@qq.com>
2021-08-30 15:24:55 +0800

    使用logging规范日志输出

--- CUC_Report_by_Requests/run_all.py
+++ CUC_Report_by_Requests/run_all.py
@@ -1,8 +1,13 @@
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 file_list = os.listdir(os.getcwd())
-print(file_list)
+logging.info(file_list)
 for filename in file_list:
     if os.path.isfile(filename) and filename.endswith('.py') and filename.find("run") == -1:
-        print(filename,'\n')
+        logging.info(filename)
         os.system('/root/anaconda3/bin/python {}'.format(filename))
