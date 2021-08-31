73e3b7f4480eb1d2def8831ba6a548df30cbc50e
Dewey <deweyshi@qq.com>
2021-08-31 10:57:26 +0800

    根据日志判断成功或失败状态

--- send_email.sh
+++ send_email.sh
@@ -2,7 +2,17 @@
 CUC_Report_by_Requests_daily_log=$(tail -n 5 /root/CUC_Report_by_Requests/log.txt)
 SICNU_Report_Epidemic_daily_log=$(tail -n 14 /root/SICNU_Report_Epidemic/log.txt)
 time=$(date "+%Y-%m-%d")
-echo $CUC_Report_by_Requests_daily_log
-echo $SICNU_Report_Epidemic_daily_log
-mail -s "Report from SICNU_Report_Epidemic" xxxxxxx@xx.com -A /root/SICNU_Report_Epidemic/${time}-20200208802.png -A /root/SICNU_Report_Epidemic/${time}-20200204801.png <<< $SICNU_Report_Epidemic_daily_log
-mail -s "Report from CUC_Report_by_Requests" xxxxxxx@xx.com <<< $CUC_Report_by_Requests_daily_log
+if [[ $CUC_Report_by_Requests_daily_log == *'"check_code":0'*"今日健康日报数据提交成功"*'"check_code":0'* ]]
+then
+	email_subject_CUC="[Success]"
+else
+	email_subject_CUC="[Failure]"
+fi
+if [[ $SICNU_Report_Epidemic_daily_log == *'2020xxxxxx-succeed'*'2020xxxxxx-succeed'* ]]
+then
+	email_subject_SICNU="[Success]"
+else
+	email_subject_SICNU="[Failure]"
+fi
+mail -s ${email_subject_SICNU}"SICNU_Report_Epidemic" xxxxxxx@xx.com -A /root/SICNU_Report_Epidemic/${time}-2020xxxxxx.png -A /root/SICNU_Report_Epidemic/${time}-2020xxxxxx.png <<< $SICNU_Report_Epidemic_daily_log
+mail -s ${email_subject_CUC}"CUC_Report_by_Requests" xxxxxxxx@xx.com <<< $CUC_Report_by_Requests_daily_log
